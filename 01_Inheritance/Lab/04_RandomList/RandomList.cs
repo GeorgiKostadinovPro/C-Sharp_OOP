9d62829d919cb4a2c9e719f2c6e87d3f10db9948
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-17 14:10:07 +0300

    Add files via upload

---
+++ 01_Inheritance/Lab/04_RandomList/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private readonly Random random;
+
+        public RandomList()
+        {
+            this.random = new Random();
+        }
+
+        public string RandomString()
+        {
+            int index = this.random.Next(0, this.Count);
+            string randomElement = this[index];
+            Remove(randomElement);
+
+            return randomElement;
+        }
+    }
+}
---
+++ 01_Inheritance/Lab/04_RandomList/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+
+            randomList.Add("George");
+            randomList.Add("Lyubo");
+            randomList.Add("Kris");
+
+            Console.WriteLine(string.Join(", ", randomList));
+
+            string randomElement = randomList.RandomString();
+
+            Console.WriteLine(randomElement);
+            Console.WriteLine(string.Join(", ", randomList));
+        }
+    }
+}
