2c3d65e64f2acd7a499605a1f0aa45ec6c9b69c1
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-17 14:10:22 +0300

    Add files via upload

---
+++ 01_Inheritance/Lab/05_StackOfStrings/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            Console.WriteLine(stackOfStrings.IsEmpty());
+
+            string[] strings = { "George", "Lyubo", "Kris" };
+
+            stackOfStrings.AddRange(strings);
+
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            Console.WriteLine(string.Join(", ", stackOfStrings));
+        }
+    }
+}
