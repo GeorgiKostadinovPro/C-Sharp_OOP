43d69fc81be3f4ea7c40fd83b18d7479ba1b76ac
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-19 13:57:53 +0300

    Add files via upload

---
+++ 02_Encapsulation/Lab/04_Team/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            List<Person> persons = new List<Person>();
+            Team team = new Team("SoftUni");
+
+            for (int i = 0; i < lines; i++)
+            {
+                try
+                {
+                    string[] cmdArgs = Console.ReadLine()
+                       .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    
+                    string firstName = cmdArgs[0];
+                    string lastName = cmdArgs[1];
+                    int age = int.Parse(cmdArgs[2]);
+                    decimal salary = decimal.Parse(cmdArgs[3]);
+                    
+                    Person person = new Person(firstName, lastName, age, salary);
+                    persons.Add(person);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            foreach (Person player in persons)
+            {
+                team.AddPlayer(player);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
---
+++ 02_Encapsulation/Lab/04_Team/Team.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private readonly ICollection<Person> firstTeam;
+        private readonly ICollection<Person> reserveTeam;
+
+        private Team()
+        {
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public Team(string name)
+            : this()
+        {
+            this.Name = name;
+        }
+
+        public string Name 
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam 
+            => (IReadOnlyCollection<Person>)this.firstTeam;
+
+        public IReadOnlyCollection<Person> ReserveTeam 
+            => (IReadOnlyCollection<Person>)this.reserveTeam;
+
+        public void AddPlayer(Person player)
+        {
+            if (player.Age < 40)
+            {
+                this.firstTeam.Add(player);
+            }
+            else 
+            {
+                this.reserveTeam.Add(player);
+            }
+        }
+    }
+}
