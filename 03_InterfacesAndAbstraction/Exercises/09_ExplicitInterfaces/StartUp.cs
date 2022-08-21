71840265327b76a850bb0b06bea8b939d9bfd7a0
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-22 00:04:03 +0300

    Add files via upload

---
+++ 03_InterfacesAndAbstraction/Exercises/09_ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using ExplicitInterfaces.Models;
+using ExplicitInterfaces.Models.Contracts;
+using System;
+
+namespace ExplicitInterfaces
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string line = string.Empty;
+
+            while ((line = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = cmdArgs[0];
+                string country = cmdArgs[1];
+                int age = int.Parse(cmdArgs[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+                IPerson person = citizen;
+                IResident resident = citizen;
+
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
