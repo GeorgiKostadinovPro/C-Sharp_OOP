4e978b36c56c4ac10eaaa3c9c8d65796feb15484
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-23 15:55:41 +0300

    Business Logic 150/150 points

---
+++ MyOOPExam_22_August_2022/02_BusinessLogic/Repositories/BookingRepository.cs
@@ -0,0 +1,32 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookingApp.Repositories
+{
+    public class BookingRepository : IRepository<IBooking>
+    {
+        private readonly ICollection<IBooking> models;
+        public BookingRepository()
+        {
+            this.models = new List<IBooking>();
+        }
+        public void AddNew(IBooking model)
+        {
+            this.models.Add(model);
+        }
+
+        public IReadOnlyCollection<IBooking> All()
+        {
+            return (IReadOnlyCollection<IBooking>)this.models;
+        }
+
+        public IBooking Select(string criteria)
+        {
+            return this.models.FirstOrDefault(m => m.BookingNumber.ToString() == criteria);
+        }
+    }
+}
