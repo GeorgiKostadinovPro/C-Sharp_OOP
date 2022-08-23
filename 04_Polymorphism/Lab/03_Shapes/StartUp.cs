07942f3200ffd6849a20181453f526485b324fac
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-23 15:48:51 +0300

    Add files via upload

---
+++ 04_Polymorphism/Lab/03_Shapes/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {   
+            double radius = double.Parse(Console.ReadLine());
+            Shape circle = new Circle(radius);
+
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.Draw());
+
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            Shape rectangle = new Rectangle(width, height);
+
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.Draw());
+        }
+    }
+}
