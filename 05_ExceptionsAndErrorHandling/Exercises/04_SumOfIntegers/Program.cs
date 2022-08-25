0bc39b1a5439ca7c561a0588cb2707b1e4468b3a
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-25 15:16:13 +0300

    Add files via upload

---
+++ 05_ExceptionsAndErrorHandling/Exercises/04_SumOfIntegers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SumOfIntegers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine()
+                .Split(' ');
+
+            int totalIntegersSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                string element = numbers[i];
+
+                try
+                {
+                    int number = int.Parse(element);
+
+                    totalIntegersSum += number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{element}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{element}' is out of range!");
+                }
+                finally 
+                { 
+                    Console.WriteLine($"Element '{element}' processed - current sum: {totalIntegersSum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {totalIntegersSum}");
+        }
+    }
+}
---
+++ 05_ExceptionsAndErrorHandling/Exercises/06_MoneyTransactions/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoneyTransactions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<int, double> bankAccounts = new Dictionary<int, double>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] accountInfo = input[i]
+                    .Split('-', StringSplitOptions.RemoveEmptyEntries);
+
+                int accountNumber = int.Parse(accountInfo[0]);
+                double accountBalance = double.Parse(accountInfo[1]);
+
+                bankAccounts.Add(accountNumber, accountBalance);
+            }
+
+            string line = string.Empty;
+
+            while ((line = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string cmd = cmdArgs[0];
+                int accountNumber = int.Parse(cmdArgs[1]);
+                double sum = double.Parse(cmdArgs[2]);
+
+                try
+                {
+                    if (cmd == "Deposit")
+                    {
+                        bankAccounts[accountNumber] += sum;
+                    }
+                    else if (cmd == "Withdraw")
+                    {
+                        if (sum > bankAccounts[accountNumber])
+                        {
+                            throw new OverflowException("Insufficient balance!");
+                        }
+
+                        bankAccounts[accountNumber] -= sum;
+                    }
+                    else 
+                    {
+                        throw new ArgumentException("Invalid command!");
+                    }
+
+                    Console.WriteLine($"Account {accountNumber} has new balance: {bankAccounts[accountNumber]:f2}");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (KeyNotFoundException)
+                {
+                    Console.WriteLine("Invalid account!");
+                }
+                
+                finally
+                {
+                    Console.WriteLine("Enter another command");
+                }
+            }
+        }
+    }
+}
