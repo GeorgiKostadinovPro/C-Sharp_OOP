ea1de4a135297502ed5b308a6a47ab676b53d8b9
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-26 21:55:30 +0300

    Add files via upload

---
+++ 06_ReflectionАndAttributes/Lab/06_CodeTracker/Tracker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        { 
+            Type startUp = typeof(StartUp);
+
+            MethodInfo[] methods = startUp.GetMethods(
+                BindingFlags.Public
+                | BindingFlags.NonPublic
+                | BindingFlags.Instance 
+                | BindingFlags.Static);
+
+            foreach (var method in methods)
+            {
+                ICollection<AuthorAttribute> attributes = 
+                    (ICollection<AuthorAttribute>)method.GetCustomAttributes<AuthorAttribute>();
+
+                if (attributes.Count > 0)
+                {
+                    foreach (var attribute in attributes)
+                    {
+                         Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+                }
+            } 
+        }
+    }
+}
