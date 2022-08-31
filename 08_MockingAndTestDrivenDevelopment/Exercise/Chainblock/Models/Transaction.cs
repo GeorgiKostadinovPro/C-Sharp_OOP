9a118826c31100af8c68798e75c9af4337d66175
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-31 18:31:19 +0300

    Need to finish the blockchain class functionality

---
+++ 08_MockingAndTestDrivenDevelopment/Exercise/Chainblock/Models/Transaction.cs
@@ -0,0 +1,76 @@
+﻿using Chainblock.Contracts;
+using System;
+
+namespace Chainblock.Models
+{
+    public class Transaction : ITransaction
+    {
+        private string from;
+        private string to;
+        private double amount;
+
+        public Transaction(int id, TransactionStatus status, string from, string to, double amount)
+        {
+            this.Id = id;
+            this.Status = status;
+            this.From = from;
+            this.To = to;
+            this.Amount = amount;
+        }
+
+        public int Id { get; set; }
+
+        public TransactionStatus Status { get; set; }
+
+        public string From
+        {
+            get
+            {
+                return this.from;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Sender name cannot be null or whitespace string!");
+                }
+
+                this.from = value;
+            }
+        }
+
+        public string To
+        {
+            get
+            {
+                return this.to;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Receiver name cannot be null or whitespace string!");
+                }
+
+                this.to = value;
+            }
+        }
+
+        public double Amount
+        {
+            get
+            {
+                return this.amount;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Transaction amount must be a positive number!");
+                }
+
+                this.amount = value;
+            }
+        }
+    }
+}
