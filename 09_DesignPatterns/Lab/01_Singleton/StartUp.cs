6f8d37575aee6693826f466765ce5163caeb9ad7
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-09-09 15:43:31 +0300

    Add files via upload

---
+++ 09_DesignPatterns/Lab/01_Singleton/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SingletonDataContainer db = SingletonDataContainer.Instance;
+
+            int sofiaPopulation = db.GetPopulation("Sofia");
+            Console.WriteLine($"Sofia - {sofiaPopulation}");
+        }
+    }
+}
