64449a4cb317cbae8c7c8efc965bee445ed1c552
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-09-10 13:42:43 +0300

    a simple project using the Facade pattern

---
+++ 09_DesignPatterns/Lab/02_Facade/CarAddresBuilder.cs
@@ -0,0 +1,24 @@
+﻿namespace Facade
+{
+    public class CarAddresBuilder : CarBuilderFacade
+    {
+        public CarAddresBuilder(Car car)
+        {
+            this.Car = car;
+        }
+
+        public CarAddresBuilder InCity(string cityName)
+        {
+            this.Car.City = cityName;
+
+            return this;
+        }
+
+        public CarAddresBuilder AtAddress(string address)
+        {
+            this.Car.Address = address;
+
+            return this;
+        }
+    }
+}
---
+++ 09_DesignPatterns/Lab/02_Facade/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Facade
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new CarBuilderFacade()
+                .Info
+                  .WithType("BMW")
+                  .WithColor("Black")
+                  .WithNumberOfDoors(4)
+                .Address
+                  .InCity("Leipzig")
+                  .AtAddress("BMW-Allee 1, 04349 Leipzig, Germany")
+                .Build();
+
+            Console.WriteLine(car.ToString());
+        }
+    }
+}
