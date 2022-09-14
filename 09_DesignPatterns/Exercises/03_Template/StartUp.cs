e4b9620a1a248b7b81057f7b482f77adebd33948
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-09-14 15:14:46 +0300

    Add files via upload

---
+++ 09_DesignPatterns/Exercises/03_Template/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Template
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Sourdough sourdoughBread = new Sourdough();
+            sourdoughBread.Make();
+
+            TwelveGrain twelveGrainBread = new TwelveGrain();
+            twelveGrainBread.Make();
+
+            WholeWheat wholeWheatBread = new WholeWheat();
+            wholeWheatBread.Make();
+        }
+    }
+}
---
+++ 09_DesignPatterns/Exercises/03_Template/WholeWheat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Template
+{
+    public class WholeWheat : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine($"Gathering ingredients for WholeWheat bread.");
+        }
+
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the Whole Wheat bread. (15 minutes)");
+        }
+
+        public override void Slice()
+        {
+            base.Slice();
+
+            Console.WriteLine($"The Whole Wheat bread should be cut on 10 slices.");
+        }
+    }
+}
