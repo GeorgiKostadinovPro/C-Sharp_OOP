fd742c860e571385060b21bd6078b4ff7d9d5f83
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-09-14 14:12:35 +0300

    Add files via upload

---
+++ 09_DesignPatterns/Exercises/02_Composite/CompositeGift.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Composite
+{
+    public class CompositeGift : GiftBase, IGiftOperations
+    {
+        private readonly ICollection<GiftBase> gifts;
+
+        public CompositeGift(string name, decimal price) 
+            : base(name, price)
+        {
+            this.gifts = new List<GiftBase>();
+        } 
+        
+        public void Add(GiftBase gift)
+        {
+            this.gifts.Add(gift);
+        }
+
+        public bool Remove(GiftBase gift)
+        {
+            return this.gifts.Remove(gift);
+        }
+        
+        public override decimal CalculateTotalPrice()
+        {
+            decimal totalPrice = 0.0m;
+
+            Console.WriteLine($"{this.Name} contains the following products with prices:");
+
+            foreach (var gift in this.gifts)
+            {
+                totalPrice += gift.CalculateTotalPrice();
+            }
+
+            return totalPrice;
+        }
+    }
+}
---
+++ 09_DesignPatterns/Exercises/02_Composite/GiftBase.cs
@@ -0,0 +1,40 @@
+﻿namespace Composite
+{
+    public abstract class GiftBase
+    {
+        private string name;
+        private decimal price;
+
+        public GiftBase(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name
+        {
+            get 
+            { 
+                return this.name; 
+            }
+            private set 
+            { 
+                this.name = value; 
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            private set
+            {
+                this.price = value;
+            }
+        }
+
+        public abstract decimal CalculateTotalPrice(); 
+    }
+}
